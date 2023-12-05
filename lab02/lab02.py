b755ee6788bc54d0055fd7cdcf4ee66d9c58b0e8
“tsingbochun” <tsingbochun@gmail.com>
2023-12-05 21:20:12 +0800

    # Q7: Make Your Own Lambdas finish
    # Q7: Make Your Own Lambdas finish

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -186,3 +186,36 @@ def curry(func):
         return h
     return g
 
+# Q7: Make Your Own Lambdas
+def f1():
+    """
+    >>> f1()
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    return 3
+
+def f2():
+    """
+    >>> f2()()
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda: 3
+
+def f3():
+    """
+    >>> f3()(3)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda x: x
+
+def f4():
+    """
+    >>> f4()()(3)()
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda: lambda x: lambda: x
+
