0947460ece554a8a82f27be99d7ea98c0b5a8460
“tsingbochun” <tsingbochun@gmail.com>
2023-12-05 21:06:40 +0800

    curry finished
    curry finished

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -164,3 +164,25 @@ def make_keeper(n):
             i += 1
     return myfunction
 
+def curry(func):
+    """
+    Returns a Curried version of a two-argument function FUNC.
+    >>> from operator import add, mul, mod
+    >>> curried_add = curry(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    >>> curried_mul = curry(mul)
+    >>> mul_5 = curried_mul(5)
+    >>> mul_5(42)
+    210
+    >>> curry(mod)(123)(10)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    def g(x):
+        def h(y):
+            return func(x, y)
+        return h
+    return g
+
