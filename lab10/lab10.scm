96d14f3138e9e666435c930c2148f5793ea82caa
“tsingbochun” <tsingbochun@gmail.com>
2023-12-29 14:09:27 +0800

    Q3: Make Adder
    Q3: Make Adder

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,8 +1,8 @@
 (define (over-or-under num1 num2) (if (< num1 num2) -1 (if (= num1 num2) 0 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (f (g x)))
 
 (define (repeat f n) 'YOUR-CODE-HERE)
 
